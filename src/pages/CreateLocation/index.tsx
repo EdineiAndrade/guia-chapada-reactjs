ad0b4757aab34c25ad2c2204ecc9a96a0075a2d6
EdineiAndrade <dinei22.ba@gmail.com>
2020-11-27 20:15:30 -0300

    'pagina-2'

--- src/pages/CreateLocation/index.tsx
+++ src/pages/CreateLocation/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import {FiLogIn} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
-
+import ValePati from '../../images/capa-vale-pati.jpg'
 import './styles2.css';
 
 const CreateLocation: React.FC = () =>{
     return(
         <div id="page-home">
             <div className="content">
-             <h1>Pag2</h1>
+            <header>
+                  <img src={ValePati} alt="Reciclagem"/>
+              </header>
+              <main>
+                  <h1>Coleta</h1>
+                  <p>Reciclagem </p>
+                  
                 <Link to="/">
                    <span>
                         <FiLogIn/>
                     </span>
 
                 </Link>
+                </main>
             </div>
         </div>
     );
